2c6889c57e3e67ed744019a40348c981dbb61406
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2018-11-03 02:55:54 -0400

    Update FilesOnRobot/SweepServo/SweepServo.ino

--- FilesOnRobot/SweepServo/SweepServo.ino
+++ FilesOnRobot/SweepServo/SweepServo.ino
@@ -1,12 +1,3 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
 
 Servo myservo;  
@@ -17,18 +8,17 @@ int current_pos = 0;    // variable to store the servo current's position
 void setup() {
   myservo.attach(SERVO_PIN);
   servo_write(0, 50);
+}
+
+void loop() {
   rotate(180, 15);
   rotate(-180, 15);
 }
 
-void loop() {
-
-}
-
 void servo_write(int value, int time_delay) {
   myservo.write(value);
-  delay(time_delay);
   current_pos = value;
+  delay(time_delay);
 }
 
 void rotate(int degree, int time_delay) {
