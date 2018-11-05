1fc8c1e81ba2ba44f6765cfb67403bfb59630794
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2018-11-04 21:03:25 -0500

    Update FilesOnRobot/LightAndSound/LightAndSound.ino
    - Put Sound and Light sensing into functions
    - Change while statement in Sound sensing system to if statement so that it will not block the thread
    - Decrease the sound threshold
    - Fix orientation calculation (angle_degree) in Sound sensing system
    - Create rotateCommand() used in Sound sensing system and use a better delay time for rotation

--- FilesOnRobot/LightAndSound/LightAndSound.ino
+++ FilesOnRobot/LightAndSound/LightAndSound.ino
@@ -1,9 +1,6 @@
 /****************************************
-Example Sound Level Sketch for the 
-Adafruit Microphone Amplifier
-
-This code prioritized Sound. 
-If the robot gets a sound input, a flag will be set to "TRUE" and it will skip the light sensing for a little while.
+  This code prioritized Sound.
+  If the robot gets a sound input, a flag will be set to "TRUE" and it will skip the light sensing for a little while.
 ****************************************/
 // Mic 1 @ 120 degrees connects to A1
 // Mic 2 @ 0 degrees connects to A2
@@ -15,7 +12,7 @@ If the robot gets a sound input, a flag will be set to "TRUE" and it will skip t
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 //includes the motorshield required libraries
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 //Creates the motorshield object
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
@@ -28,7 +25,7 @@ unsigned int sample;
 unsigned int sample2;
 unsigned int sample3;
 unsigned int angle_deg = 0;
-int threshold_sound = 200;
+int threshold_sound = 50;
 unsigned long moveMillis = millis(); // Starts counting time for how long an angle has been given
 
 
@@ -44,12 +41,12 @@ float ymult = 0;
 const int threshold = 10; // Lighting threshold
 const int dspeed = 40;// default speed
 const int xcal = -44; // Need to calibrate xcal and ycal under the normal lighting condition
-const int ycal = -38; 
+const int ycal = -38;
 const float forwardbuffer = .2;
 
-void setup() 
+void setup()
 {
-  
+
   // MOTOR SHIELD SETUP
   AFMS.begin();
   leftMotor->setSpeed(dspeed);
@@ -72,198 +69,216 @@ void setup()
   digitalWrite(12, HIGH);
   digitalWrite(13, HIGH);
   angle_deg = 0;
-
-   
 }
 
 
-void loop() 
+void loop()
 {
-  // SOUND SENSING
-   unsigned long startMillis= millis();  // Start of sample window
-   unsigned int peakToPeak = 0;   // peak-to-peak level
-   unsigned int peakToPeak2 = 0; // Mic 2
-   unsigned int peakToPeak3 = 0; // Mic 3
-
-   unsigned int signalMax = 0; // Mic 1
-   unsigned int signalMin = 1024;
-   unsigned int signalMax2 = 0; // Mic 2
-   unsigned int signalMin2 = 1024;
-   unsigned int signalMax3 = 0; // Mic 3
-   unsigned int signalMin3 = 1024;
-
-
-   // collect data for 50 mS
-   while (millis() - startMillis < sampleWindow)
-   {
-      
-      sample = analogRead(A1);
-      sample2 = analogRead(A2);
-      sample3 = analogRead(A0);
-      // Mic 1
-      if (sample < 1024)  // toss out spurious readings
-      {
-         if (sample > signalMax)
-         {
-            signalMax = sample;  // save just the max levels
-         }
-         else if (sample < signalMin)
-         {
-            signalMin = sample;  // save just the min levels
-         }
-      }
-      // Mic 2
-      if (sample2 < 1024)  // toss out spurious readings
-      {
-         if (sample2 > signalMax2)
-         {
-            signalMax2 = sample2;  // save just the max levels
-         }
-         else if (sample2 < signalMin2)
-         {
-            signalMin2 = sample2;  // save just the min levels
-         }
-      }
-      // Mic 3
-      if (sample3 < 1024)  // toss out spurious readings
-      {
-         if (sample3 > signalMax3)
-         {
-            signalMax3 = sample3;  // save just the max levels
-         }
-         else if (sample3 < signalMin3)
-         {
-            signalMin3 = sample3;  // save just the min levels
-         }
-      }
-   }
-   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-   double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
-   // Mic 2
-   peakToPeak2 = signalMax2 - signalMin2;  // max - min = peak-peak amplitude
-   double volts2 = (peakToPeak2 * 5.0) / 1024;  // convert to volts
-   // Mic 3
-   peakToPeak3 = signalMax3 - signalMin3;  // max - min = peak-peak amplitude
-   double volts3 = (peakToPeak3 * 5.0) / 1024;  // convert to volts
-
-//   Serial.println(volts*100);
-   Serial.println("Mic 1: "); Serial.print(volts*1000); Serial.print("  ");
-   Serial.print("Mic 2: "); Serial.print(volts2*1500); Serial.print("  ");
-   Serial.print("Mic 3: "); Serial.print(volts3*1000); Serial.print("  ");
-
-// Maybe later, change to if(volts*1000+volts2*1500+volts3*1000 > threshold*3)
-   if(volts*1000 > threshold_sound || volts2*1500>threshold_sound || volts3*1000>threshold_sound){
-    if(volts*1000> volts2*1500){
-      if(volts3*1000>volts2*1500){
-        if(volts3*1000>volts*1000){
-          // Value order is 312
-          angle_deg = 340;
-        }
-        else{
-          // Value order is 132
-          angle_deg = 200;
-        }
-      }
-      else{
-        // Value order is 123
-        angle_deg = 100;
-      }
-    }
-    else{
-      if(volts3*1000>volts2*1500){
-        
-      }
-      else{
-        if(volts3*1000>volts2*1500){
-          // Value order is 321
-          angle_deg = 260;
-        }
-        else{
-          if(volts3*1000>volts*1000){
-            // Value order is 231
-            angle_deg = 220;
-          }
-          else{
-            // Value order is 213
-            angle_deg = 140;
-          }
-        }
-      }
-    }
-   };
-   
-   if(angle_deg > 0){
-    rightMotor->setSpeed(50);
-    leftMotor->setSpeed(50);
-    leftMotor->run(BACKWARD);
-    rightMotor->run(FORWARD);
-    delay(angle_deg*3.333);
-    stopCommand();
-    leftMotor->run(FORWARD);
-    rightMotor->run(FORWARD);
-    delay(1000);
-    stopCommand();
-    angle_deg = 0;
-   }
-   
-  //LIGHT SENSING
-  y = analogRead(front) - (analogRead(left) + analogRead(right))/2 + ycal;
-    x = analogRead(left) - analogRead(right) + xcal;
-    // positive y = forward, positive x = left
-    if (abs(x) > threshold or abs(y) > threshold) {
-      // Scale down x and y
-      xmult = abs(x)/100.0;
-      ymult = abs(y)/50.0;
-
-      // xmult is from 0 to 1
-      if (xmult > 1){
-        xmult = 1;
-      }
-
-      // ymult is from 0.2 to 1
-      ymult += forwardbuffer;
-      if (ymult > 1){
-        ymult = 1;
-      }
-
-      // speeds are from 0 to dspeed
-      int turnspeed = dspeed * ymult * (1 - xmult); // turnspeed <= forwardspeed
-      int forwardspeed = dspeed * ymult;
-    
-      runCommand(forwardspeed, turnspeed);
-    } else {
-      stopCommand();
-    }
-    //printValues();
-  
-  
-
+  senseSound(false);
+  senseLight(false);
 }
 
-  // Helper function to set the wheels' speeds
-  void runCommand(int forwardspeed, int turnspeed) {
-    if (x > 0){
-      // Turn left
-      rightMotor->setSpeed(forwardspeed);
-      leftMotor->setSpeed(turnspeed);
-    } else {
-      // Turn right
-      rightMotor->setSpeed(turnspeed);
-      leftMotor->setSpeed(forwardspeed);    
+
+// SOUND SENSING
+void senseSound(bool shouldPrint) {
+  static unsigned long startMillis;  // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
+  unsigned int peakToPeak2 = 0; // Mic 2
+  unsigned int peakToPeak3 = 0; // Mic 3
+
+  static unsigned int signalMax; // Mic 1
+  static unsigned int signalMin;
+  static unsigned int signalMax2; // Mic 2
+  static unsigned int signalMin2;
+  static unsigned int signalMax3; // Mic 3
+  static unsigned int signalMin3;
+
+
+  if (startMillis == 0) { // Re-initialize everything
+    startMillis = millis();
+    signalMax = 0;
+    signalMax2 = 0;
+    signalMax3 = 0;
+    signalMin = 1024;
+    signalMin2 = 1024;
+    signalMin3 = 1024;
+  }
+  // collect data for 50 mS
+  if (millis() - startMillis < sampleWindow)
+  {
+    sample = analogRead(A1);
+    sample2 = analogRead(A2);
+    sample3 = analogRead(A0);
+    // Mic 1
+    if (sample < 1024)  // toss out spurious readings
+    {
+      if (sample > signalMax)
+      {
+        signalMax = sample;  // save just the max levels
+      }
+      else if (sample < signalMin)
+      {
+        signalMin = sample;  // save just the min levels
+      }
     }
-    leftMotor->run(y > 0 ? FORWARD : BACKWARD); // Conditional tenary operator
-    rightMotor->run(y > 0 ? FORWARD : BACKWARD);
-  }
+    // Mic 2
+    if (sample2 < 1024)  // toss out spurious readings
+    {
+      if (sample2 > signalMax2)
+      {
+        signalMax2 = sample2;  // save just the max levels
+      }
+      else if (sample2 < signalMin2)
+      {
+        signalMin2 = sample2;  // save just the min levels
+      }
+    }
+    // Mic 3
+    if (sample3 < 1024)  // toss out spurious readings
+    {
+      if (sample3 > signalMax3)
+      {
+        signalMax3 = sample3;  // save just the max levels
+      }
+      else if (sample3 < signalMin3)
+      {
+        signalMin3 = sample3;  // save just the min levels
+      }
+    }
+  } else {
+    peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+    double volts = 1000 * (peakToPeak * 5.0) / 1024;  // convert to volts
+    // Mic 2
+    peakToPeak2 = signalMax2 - signalMin2;  // max - min = peak-peak amplitude
+    double volts2 = 1500 * (peakToPeak2 * 5.0) / 1024;  // convert to volts
+    // Mic 3
+    peakToPeak3 = signalMax3 - signalMin3;  // max - min = peak-peak amplitude
+    double volts3 = 1000 * (peakToPeak3 * 5.0) / 1024;  // convert to volts
 
-  // Helper function to stop both wheels
-  void stopCommand() {
-    leftMotor->run(RELEASE);
-    rightMotor->run(RELEASE);
-  }
+    // Maybe later, change to if(volts+volts2+volts3 > threshold*3)
+    if (volts > threshold_sound || volts2 > threshold_sound || volts3 > threshold_sound) {
+      if (volts > volts2) {
+        if (volts3 > volts2) {
+          if (volts3 > volts) {
+            // Value order is 312
+            angle_deg = 270;
+          }
+          else {
+            // Value order is 132
+            angle_deg = 330;
+          }
+        }
+        else {
+          // Value order is 123
+          angle_deg = 30;
+        }
+      }
+      else {
+        if (volts2 > volts3) {
+          if (volts > volts3) {
+            // Value order is 213
+            angle_deg = 90;
+          }
+          else {
+            // Value order is 231
+            angle_deg = 150;
+          }
+        } else {
+          // Value order is 321
+          angle_deg = 210;
+        }
+      }
+    };
 
-  // Helper function to print whatever values we want
-  void printValues() {
-    Serial.print(xmult);
+    if (shouldPrint && false) {
+      Serial.print("Mic 1: "); Serial.print(volts); Serial.print("\t");
+      Serial.print("Mic 2: "); Serial.print(volts2); Serial.print("\t");
+      Serial.print("Mic 3: "); Serial.print(volts3); Serial.println("\t");
+      Serial.print("Sound detected: "); Serial.println(angle_deg);
+    }
+
+    if (angle_deg > 0) {
+      rightMotor->setSpeed(50);
+      leftMotor->setSpeed(50);
+      rotateCommand(angle_deg - 180); // delay() goes in here
+      stopCommand();
+      leftMotor->run(FORWARD);
+      rightMotor->run(FORWARD);
+      delay(1000);
+      stopCommand();
+      angle_deg = 0;
+    }
+    startMillis = 0; // Flag 0 to reset everything
+  }
+}
+
+
+//LIGHT SENSING
+void senseLight(bool shouldPrint) {
+  y = analogRead(front) - (analogRead(left) + analogRead(right)) / 2 + ycal;
+  x = analogRead(left) - analogRead(right) + xcal;
+  // positive y = forward, positive x = left
+  if (abs(x) > threshold or abs(y) > threshold) {
+    // Scale down x and y
+    xmult = abs(x) / 100.0;
+    ymult = abs(y) / 50.0;
+
+    // xmult is from 0 to 1
+    if (xmult > 1) {
+      xmult = 1;
+    }
+
+    // ymult is from 0.2 to 1
+    ymult += forwardbuffer;
+    if (ymult > 1) {
+      ymult = 1;
+    }
+
+    // speeds are from 0 to dspeed
+    int turnspeed = dspeed * ymult * (1 - xmult); // turnspeed <= forwardspeed
+    int forwardspeed = dspeed * ymult;
+
+    runCommand(forwardspeed, turnspeed);
+  } else {
+    stopCommand();
+  }
+  if (shouldPrint) {
+    Serial.print(analogRead(front));
     Serial.print("\t");
-    Serial.println(ymult);
+    Serial.print(analogRead(right));
+    Serial.print("\t");
+    Serial.println(analogRead(left));
   }
+}
+
+
+// Helper function to set the wheels' speeds
+void runCommand(int forwardspeed, int turnspeed) {
+  if (x > 0) {
+    // Turn left
+    rightMotor->setSpeed(forwardspeed);
+    leftMotor->setSpeed(turnspeed);
+  } else {
+    // Turn right
+    rightMotor->setSpeed(turnspeed);
+    leftMotor->setSpeed(forwardspeed);
+  }
+  leftMotor->run(y > 0 ? FORWARD : BACKWARD); // Conditional tenary operator
+  rightMotor->run(y > 0 ? FORWARD : BACKWARD);
+}
+
+
+// Helper function to set the wheels' speeds
+void rotateCommand(int degree) {
+  Serial.println(degree);
+  leftMotor->run(degree > 0 ? FORWARD : BACKWARD);
+  rightMotor->run(degree > 0 ? BACKWARD : FORWARD);
+  delay(abs(degree) * 700.0 / 180); // 700 ms rotates 180 degrees
+}
+
+
+// Helper function to stop both wheels
+void stopCommand() {
+  leftMotor->run(RELEASE);
+  rightMotor->run(RELEASE);
+}
