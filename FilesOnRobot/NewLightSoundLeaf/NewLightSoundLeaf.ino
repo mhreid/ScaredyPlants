60ea67c87f3be23572225490e958f2d1f01f4833
mhreid <micahhelenreid@gmail.com>
2018-12-01 19:30:36 -0500

    minor updates

--- FilesOnRobot/NewLightSoundLeaf/NewLightSoundLeaf.ino
+++ FilesOnRobot/NewLightSoundLeaf/NewLightSoundLeaf.ino
@@ -18,6 +18,7 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 //Creates the motorshield object
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *leafMotor = AFMS.getMotor(4);
 
 
 //SOUND STUFF
@@ -32,9 +33,9 @@ unsigned long moveMillis = millis(); // Starts counting time for how long an ang
 
 
 //LIGHT STUFF
-const int front = 13;
-const int right = 15;
-const int left = 11;
+const int front = A15;
+const int right = A14;
+const int left = A13;
 int x = 0;
 int y = 0;
 float xmult = 0;
@@ -42,9 +43,9 @@ float ymult = 0;
 
 const int threshold_light = 10;
 const int dspeed = 150;// default speed for wheels
-const int frontCal = 0; // Need to calibrate xcal and ycal under the normal lighting condition, make it so all == 300 in normal light
-const int leftCal = 0;
-const int rightCal = 0;
+const int frontCal = -9; // Need to calibrate xcal and ycal under the normal lighting condition, make it so all == 800 in normal light
+const int leftCal = -10;
+const int rightCal = -25;
 const float forwardbuffer = .2;
 
 //LEAF SERVO STUFF
@@ -59,13 +60,13 @@ Servo leaves3;
 int leavespos1 = pos; 
 int leavespos2 = pos;
 int leavespos3 = pos;
-
 void setup()
 {
   // MOTOR SHIELD SETUP
   AFMS.begin();
   leftMotor->setSpeed(dspeed);
   rightMotor->setSpeed(dspeed);
+  leafMotor->setSpeed(55);
   Serial.begin(9600);
   angle_deg = 0;
 
@@ -74,13 +75,13 @@ void setup()
   leaves2.attach(11);
   leaves3.attach(12);
 
+
 }
 
 void loop()
 {
-
-  senseSound(false);
-  senseLight(false);
+  //senseSound(false);
+  senseLight(true);
 }
 
 
@@ -111,9 +112,9 @@ void senseSound(bool shouldPrint) {
   // collect data for 50 mS
   if (millis() - startMillis < sampleWindow)
   {
-    sample = analogRead(7);
-    sample2 = analogRead(9);
-    sample3 = analogRead(5);
+    sample = analogRead(A13);
+    sample2 = analogRead(A9);
+    sample3 = analogRead(A11);
     // Mic 1
     if (sample < 1024)  // toss out spurious readings
     {
@@ -216,9 +217,6 @@ void rotateandrun(int motor_speed, int angle) {
     if (millis() > (3000 / 140)*counter2) {
       pos -= 1;
       leaves1.write(pos); //0 means it is all the way up
-
-      leaves2.write(pos); 
-      leaves3.write(pos); 
       counter2 += 1;
     }
   }
@@ -235,14 +233,14 @@ void pullLeaves(bool up) {
     pos +=1;
     leaves1.write(pos);
     leaves2.write(pos);
-//    leaves3.write(pos);   // tell servo to go to position in variable 'pos'
+    leaves3.write(pos);   // tell servo to go to position in variable 'pos'
     delay(3);
   }
   if(pos >= uppos && up == true ){
     pos -= 1;
     leaves1.write(pos);
     leaves2.write(pos);
-//    leaves3.write(pos); // tell servo to go to position in variable 'pos'
+    leaves3.write(pos); // tell servo to go to position in variable 'pos'
     delay(3);
   }
 }
@@ -253,9 +251,7 @@ void senseLight(bool shouldPrint) {
   float frontVal = analogRead(front) + frontCal;
   float leftVal = analogRead(left) + leftCal;
   float rightVal = analogRead(right) + rightCal; 
-  Serial.println("f" + String(frontVal) + "l" + String(leftVal) + "r" + String(rightVal));
   y = frontVal - (leftVal + rightVal)/2;
-
   x = leftVal - rightVal;
   // positive y = forward, positive x = left
   if (abs(x) > threshold_light or abs(y) > threshold_light) {
@@ -275,7 +271,7 @@ void senseLight(bool shouldPrint) {
     }
     //This makes speed exponential based on proximity
     ymult *= ymult;
-    Serial.println(ymult);
+    //Serial.println(ymult);
     if(ymult > .5){
       pullLeaves(true);
     }
@@ -292,9 +288,9 @@ void senseLight(bool shouldPrint) {
     stopCommand();
   }
   if (shouldPrint) {
-    Serial.print(frontVal);
+    Serial.print(x);
     Serial.print("\t");
-    Serial.print(leftVal);
+    Serial.println(y);
     Serial.print("\t");
     Serial.println(rightVal);
   }
@@ -335,8 +331,6 @@ void rotateCommand(int degree) {
     if (millis() > abs(degree)*counter1 * 700.0 / (180 * 140)) {
       pos += 1;
       leaves1.write(pos); //0 means it is all the way up
-      leaves2.write(pos); //0 means it is all the way up
-      leaves3.write(pos); //0 means it is all the way up
       counter1 += 1;
     }
   }
